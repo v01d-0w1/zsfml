9674af256ca4c18def2d7f59ed02b773d2f2512b
z6death <tnz426.z6@gmail.com>
2025-11-01 16:05:23 +0600

    initial

--- feather.sh
+++ feather.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -c src/main.cpp -Iinclude
+g++ -c src/main.cpp -Iinclude -o src/main.o
 g++ src/main.o -o build/owl -Llib -lsfml-graphics -lsfml-window -lsfml-system
 export LD_LIBRARY_PATH=lib && ./build/owl
