03dc58ae690a722aecf6cb86a4d74ec9571ddea2
z6death <tnz426.z6@gmail.com>
2025-11-02 22:54:20 +0600

    initial

--- create_struct.sh
+++ create_struct.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-read -p "directory path(if you want in current directory leave blank):" loc_path
+read -p "directory path (if you want in current directory leave blank): " loc_path
 
-cd $loc_path
+if [ -z "$loc_path" ]; then
+    loc_path=$(pwd)
+elif [ "$loc_path" = "null" ]; then
+    loc_path=$(pwd)
+fi
 
-read -p "project_name:" prog_name
-mkdir $prog_name
-git clone https://github.com/v01d-0w1/zsfml $prog_name
+cd "$loc_path" || { echo "Error: Cannot change to directory $loc_path"; exit 1; }
 
-export $prog_name
-cd $prog_name
+read -p "project_name: " prog_name
+mkdir -p "$prog_name"
+
+git clone https://github.com/v01d-0w1/zsfml "$prog_name"
+
+cd "$prog_name" || { echo "Error: Cannot enter project directory"; exit 1; }
+
+export PROJ_NAME="$prog_name"
+
+chmod +x feather.sh
 ./feather.sh
-
