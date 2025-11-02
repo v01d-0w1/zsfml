cba6e3d475afb8fe15d4703674fde5386366010e
z6death <tnz426.z6@gmail.com>
2025-11-02 22:13:25 +0600

    initial

--- feather.sh
+++ feather.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+game_name="owl"
+if [ -n "$prog_name" ] && [ "$prog_name" != "null" ]; then
+    game_name="$prog_name"
+fi
+
+echo "Building game: $game_name"
+
 g++ -c src/main.cpp -Iinclude -o src/main.o
-g++ src/main.o -o build/owl -Llib -lsfml-graphics -lsfml-window -lsfml-system
-export LD_LIBRARY_PATH=lib && ./build/owl
+g++ src/main.o -o build/$game_name -Llib -lsfml-graphics -lsfml-window -lsfml-system
+export LD_LIBRARY_PATH=lib && ./build/$game_name
+
